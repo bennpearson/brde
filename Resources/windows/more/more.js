b2204a68f4e219be360d2de6c121b4a2aeb22aa1
Paul Fry <paul-fry@hotmail.com>
2012-01-05 15:49:04 +0000

    Scorecard styling. Also, small adjustments

--- Resources/windows/more/more.js
+++ Resources/windows/more/more.js
@@ -1,13 +1,24 @@
 var win = Titanium.UI.currentWindow;
 
+win.hideNavBar();
+
 var data = [
 { title: 'Select Course' },
 { title: 'Language' },
 { title: 'Settings' },
-{ title: 'Register' },
 { title: 'Help' }
 ];
 
-var tableView = Titanium.UI.createTableView({data:data, style:Titanium.UI.iPhone.TableViewStyle.GROUPED});
+var tableView = Titanium.UI.createTableView({data:data, style:Titanium.UI.iPhone.TableViewStyle.GROUPED, top:44});
+
+var toolbar = Titanium.UI.createView({
+	backgroundImage:'../../img/logo.jpg',
+	height: 44,
+	left: 0,
+	right: 0,
+	top:0,
+	zIndex:5
+});
 
 win.add(tableView);
+win.add(toolbar);
